a563ad1141ae5d3cda5343e79fa201595548818f
vizzaccaro <phil@netapplications.com>
2016-03-01 19:46:26 -0800

    initial

---
+++ typings/gator-web/gator-web.d.ts
@@ -0,0 +1,21 @@
+
+declare module 'gator-web' {
+    export interface IBranding {
+        productName: string,
+        companyName: string,
+        logoDarkBackground: string,
+        logoLightBackground: string,
+        logoMenu: string,
+        logoMenuSmall: string,
+        supportEmail: string,
+        salesEmail: string,
+        salesPhone: string,
+        address1: string,
+        address2: string,
+        primaryColor: string,
+        signupUrl: string,
+        postSignupUrl: string,
+        loginUrl: string,
+        postLoginUrl: string
+    }
+}
