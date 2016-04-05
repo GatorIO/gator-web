1d7da66ecde0eb0932abccd5a84c97acca32a430
vizzaccaro <phil@netapplications.com>
2016-04-04 20:06:09 -0700

    new auth

--- routes/setup.js
+++ routes/setup.js
@@ -8,23 +8,20 @@ var dashboardRoutes = require('./dashboards');
 var bookmarkRoutes = require('./bookmarks');
 var developerRoutes = require('./developer');
 var attributeRoutes = require('./attributes');
-var helpRoutes = require('./help');
 var accessTokenRoutes = require('./accessTokens');
 function setup(app, application, callback) {
     try {
         accessTokenRoutes.setup(app, application, function () {
-            helpRoutes.setup(app, application, function () {
-                apiRoutes.setup(app, application, function () {
-                    attributeRoutes.setup(app, application, function () {
-                        bookmarkRoutes.setup(app, application, function () {
-                            dashboardRoutes.setup(app, application, function () {
-                                campaignRoutes.setup(app, application, function () {
-                                    projectRoutes.setup(app, application, function () {
-                                        reportingRoutes.setup(app, application, function () {
-                                            developerRoutes.setup(app, application, function () {
-                                                segmentRoutes.setup(app, application, function () {
-                                                    callback();
-                                                });
+            apiRoutes.setup(app, application, function () {
+                attributeRoutes.setup(app, application, function () {
+                    bookmarkRoutes.setup(app, application, function () {
+                        dashboardRoutes.setup(app, application, function () {
+                            campaignRoutes.setup(app, application, function () {
+                                projectRoutes.setup(app, application, function () {
+                                    reportingRoutes.setup(app, application, function () {
+                                        developerRoutes.setup(app, application, function () {
+                                            segmentRoutes.setup(app, application, function () {
+                                                callback();
                                             });
                                         });
                                     });
--- routes/setup.ts
+++ routes/setup.ts
@@ -19,7 +19,6 @@ import dashboardRoutes = require('./dashboards');
 import bookmarkRoutes = require('./bookmarks');
 import developerRoutes = require('./developer');
 import attributeRoutes = require('./attributes');
-import helpRoutes = require('./help');
 import accessTokenRoutes = require('./accessTokens');
 
 export function setup(app: express.Application, application: IApplication, callback) {
@@ -32,34 +31,31 @@ export function setup(app: express.Application, application: IApplication, callb
 
         accessTokenRoutes.setup(app, application, function() {
 
-            helpRoutes.setup(app, application, function(){
+            apiRoutes.setup(app, application, function(){
 
-                apiRoutes.setup(app, application, function(){
+                attributeRoutes.setup(app, application, function(){
 
-                    attributeRoutes.setup(app, application, function(){
+                    bookmarkRoutes.setup(app, application, function() {
 
-                        bookmarkRoutes.setup(app, application, function() {
+                        dashboardRoutes.setup(app, application, function() {
 
-                            dashboardRoutes.setup(app, application, function() {
+                            campaignRoutes.setup(app, application, function() {
 
-                                campaignRoutes.setup(app, application, function() {
+                                projectRoutes.setup(app, application, function() {
 
-                                    projectRoutes.setup(app, application, function() {
+                                    reportingRoutes.setup(app, application, function() {
 
-                                        reportingRoutes.setup(app, application, function() {
+                                        developerRoutes.setup(app, application, function() {
 
-                                            developerRoutes.setup(app, application, function() {
-
-                                                segmentRoutes.setup(app, application, function() {
-                                                    callback();
-                                                });
+                                            segmentRoutes.setup(app, application, function() {
+                                                callback();
                                             });
                                         });
                                     });
                                 });
                             });
                         });
-                    });
+                     });
                 });
             });
         });
