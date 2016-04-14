b16033444807a2f39f99b6c09a5e77496b7a38ed
vizzaccaro <phil@netapplications.com>
2016-04-14 11:52:18 -0700

    new auth

--- routes/developer.js
+++ routes/developer.js
@@ -5,75 +5,102 @@ var gator = require('gator-score');
 function setup(app, application, callback) {
     app.get('/developer/overview', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('overview', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('overview', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+            });
         });
     });
     app.get('/developer/web/gettingstarted', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('gettingStarted', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('gettingStarted', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/events', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('events', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('events', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/ecommerce', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('ecommerceEvents', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('ecommerceEvents', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/outboundlinks', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('links', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('links', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/formposts', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('formPosts', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('formPosts', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/multipleprojects', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('multipleProjects', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('multipleProjects', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/web/javascriptapi', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('javascriptApi', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('javascriptApi', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/attributes', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('attributeList', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            attributes: api.reporting.getAttributes('all', api.reporting.AttributeTypes.all, false)
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('attributeList', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                attributes: api.reporting.getAttributes('all', api.reporting.AttributeTypes.all, false)
+            });
         });
     });
     app.get('/developer/tools/querytester', application.enforceSecure, api.authenticate, function (req, res) {
@@ -98,98 +125,125 @@ function setup(app, application, callback) {
     });
     app.get('/developer/rest', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('restOverview', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            timezones: utils.epoch.timezones
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('restOverview', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                timezones: utils.epoch.timezones
+            });
         });
     });
     app.get('/developer/querylanguage', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('queryLanguage', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            timezones: utils.epoch.timezones
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('queryLanguage', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                timezones: utils.epoch.timezones
+            });
         });
     });
     app.get('/developer/accesstokens', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('accessTokensHelp', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('accessTokensHelp', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('scoringHelp', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('scoringHelp', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring/node', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('scoringNode', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('scoringNode', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring/dotnet', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('scoringDotnet', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('scoringDotnet', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring/https', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('scoringHttps', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('scoringHttps', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring/curl', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        res.render('scoringCurl', {
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            res.render('scoringCurl', {
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
     app.get('/developer/scoring/test', application.enforceSecure, function (req, res) {
         utils.noCache(res);
-        var options = {
-            accessToken: 'cB9nC1h5OHB19ABdePAyLiJgT0BN1JMm',
-            ip: utils.ip.remoteAddress(req),
-            ua: req.headers['user-agent'],
-            referrer: req.headers['referer'],
-            url: req.hostname,
-            timeout: 900
-        };
-        if (process.env['NODE_ENV'] == 'local') {
-            options.apiHost = '127.0.0.5';
-            options.apiPort = 8080;
-            options.apiProtocol = 'http';
-            options.ip = '72.23.32.45';
-        }
-        gator.score(options, function (err, result) {
-            if (err)
-                result = err;
-            else if (!result)
-                result = { code: 500, message: 'No result returned' };
-            delete options.accessToken;
-            res.render('scoringTest', {
-                settings: utils.config.settings(),
-                application: application,
-                req: req,
-                result: result,
-                options: options
+        req['noRedirect'] = true;
+        api.authenticate(req, req, function () {
+            var options = {
+                accessToken: 'cB9nC1h5OHB19ABdePAyLiJgT0BN1JMm',
+                ip: utils.ip.remoteAddress(req),
+                ua: req.headers['user-agent'],
+                referrer: req.headers['referer'],
+                url: req.hostname,
+                timeout: 900
+            };
+            if (process.env['NODE_ENV'] == 'local') {
+                options.apiHost = '127.0.0.5';
+                options.apiPort = 8080;
+                options.apiProtocol = 'http';
+                options.ip = '72.23.32.45';
+            }
+            gator.score(options, function (err, result) {
+                if (err)
+                    result = err;
+                else if (!result)
+                    result = { code: 500, message: 'No result returned' };
+                delete options.accessToken;
+                res.render('scoringTest', {
+                    settings: utils.config.settings(),
+                    application: application,
+                    req: req,
+                    result: result,
+                    options: options
+                });
             });
         });
     });
--- routes/developer.ts
+++ routes/developer.ts
@@ -20,91 +20,145 @@ export function setup(app: express.Application, application: IApplication, callb
     app.get('/developer/overview', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('overview',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('overview',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+            });
         });
     });
 
     app.get('/developer/web/gettingstarted', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('gettingStarted',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('gettingStarted',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/events', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('events',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('events',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/ecommerce', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('ecommerceEvents',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('ecommerceEvents',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/outboundlinks', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('links',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('links',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/formposts', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('formPosts',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('formPosts',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/multipleprojects', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('multipleProjects',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('multipleProjects',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/web/javascriptapi', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('javascriptApi',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('javascriptApi',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/attributes', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('attributeList',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            attributes: api.reporting.getAttributes('all', api.reporting.AttributeTypes.all, false)
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('attributeList',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                attributes: api.reporting.getAttributes('all', api.reporting.AttributeTypes.all, false)
+            });
         });
     });
 
@@ -135,118 +189,172 @@ export function setup(app: express.Application, application: IApplication, callb
     app.get('/developer/rest', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('restOverview',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            timezones: utils.epoch.timezones
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('restOverview',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                timezones: utils.epoch.timezones
+            });
         });
     });
 
     app.get('/developer/querylanguage', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('queryLanguage',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req,
-            timezones: utils.epoch.timezones
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('queryLanguage',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req,
+                timezones: utils.epoch.timezones
+            });
         });
     });
 
     app.get('/developer/accesstokens', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('accessTokensHelp',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('accessTokensHelp',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('scoringHelp',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('scoringHelp',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring/node', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('scoringNode',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('scoringNode',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring/dotnet', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('scoringDotnet',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('scoringDotnet',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring/https', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('scoringHttps',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('scoringHttps',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring/curl', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        res.render('scoringCurl',{
-            settings: utils.config.settings(),
-            application: application,
-            req: req
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
+
+        api.authenticate(req, req, function () {
+
+            res.render('scoringCurl',{
+                settings: utils.config.settings(),
+                application: application,
+                req: req
+            });
         });
     });
 
     app.get('/developer/scoring/test', application.enforceSecure, function (req: express.Request, res: express.Response) {
         utils.noCache(res);
 
-        var options: any = {
-            accessToken: 'cB9nC1h5OHB19ABdePAyLiJgT0BN1JMm',
-            ip: utils.ip.remoteAddress(req),
-            ua: req.headers['user-agent'],
-            referrer: req.headers['referer'],
-            url: req.hostname,
-            timeout: 900
-        };
+        //  non authenticated access is allowed
+        req['noRedirect'] = true;
 
-        if (process.env['NODE_ENV'] == 'local') {
-            options.apiHost = '127.0.0.5';
-            options.apiPort =8080;
-            options.apiProtocol= 'http';
-            options.ip = '72.23.32.45';
-        }
-        gator.score(options, function(err, result) {
+        api.authenticate(req, req, function () {
 
-            if (err)
-                result = err;
-            else if (!result)
-                result = { code: 500, message: 'No result returned' };
+            var options: any = {
+                accessToken: 'cB9nC1h5OHB19ABdePAyLiJgT0BN1JMm',
+                ip: utils.ip.remoteAddress(req),
+                ua: req.headers['user-agent'],
+                referrer: req.headers['referer'],
+                url: req.hostname,
+                timeout: 900
+            };
 
-            delete options.accessToken;
+            if (process.env['NODE_ENV'] == 'local') {
+                options.apiHost = '127.0.0.5';
+                options.apiPort =8080;
+                options.apiProtocol= 'http';
+                options.ip = '72.23.32.45';
+            }
+            gator.score(options, function(err, result) {
 
-            res.render('scoringTest',{
-                settings: utils.config.settings(),
-                application: application,
-                req: req,
-                result: result,
-                options: options
+                if (err)
+                    result = err;
+                else if (!result)
+                    result = { code: 500, message: 'No result returned' };
+
+                delete options.accessToken;
+
+                res.render('scoringTest',{
+                    settings: utils.config.settings(),
+                    application: application,
+                    req: req,
+                    result: result,
+                    options: options
+                });
             });
         });
     });
