3ec270b4ef3de0619b2618a63d0a8121ff750d27
vizzaccaro <phil@netapplications.com>
2016-04-25 22:07:18 -0700

    phantom ssl

---
+++ routes/email.js
@@ -0,0 +1,18 @@
+"use strict";
+var utils = require("gator-utils");
+var api = require('gator-api');
+function setup(app, application, callback) {
+    app.post('/email/unsubscribe', function (req, res) {
+        utils.noCache(res);
+        var params = {
+            email: req.body.email,
+            listId: req.body.listId
+        };
+        api.REST.client.post('/v1/email/unsubscribe', params, function (err, apiRequest, apiResponse, result) {
+            api.REST.sendConditional(res, err, result);
+        });
+    });
+    callback();
+}
+exports.setup = setup;
+//# sourceMappingURL=email.js.map
---
+++ routes/email.ts
@@ -0,0 +1,34 @@
+/// <reference path="../typings/node/node.d.ts" />
+/// <reference path="../typings/gator-utils/gator-utils.d.ts" />
+/// <reference path="../typings/gator-api/gator-api.d.ts" />
+/// <reference path="../typings/express/express.d.ts" />
+/// <reference path="../typings/connect-flash/connect-flash.d.ts" />
+/// <reference path="../typings/restify/restify.d.ts" />
+import utils = require("gator-utils");
+import express = require('express');
+import restify = require('restify');
+import api = require('gator-api');
+import {IApplication} from "gator-web";
+
+/*
+ Set up routes - this script handles functions required for managing access tokens
+ */
+
+export function setup(app: express.Application, application: IApplication, callback) {
+
+    app.post('/email/unsubscribe', function (req: express.Request, res: express.Response) {
+        utils.noCache(res);
+
+        var params: any = {
+            email: req.body.email,
+            listId: req.body.listId
+        };
+
+        api.REST.client.post('/v1/email/unsubscribe', params, function(err, apiRequest: restify.Request, apiResponse: restify.Response, result: any) {
+            api.REST.sendConditional(res, err, result);
+        });
+    });
+
+    callback();
+}
+
