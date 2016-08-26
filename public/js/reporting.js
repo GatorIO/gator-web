6884eee314646615df0305e6b201d44c14fb5875
vizzaccaro <phil@netapplications.com>
2016-08-25 21:36:54 -0400

    component update

--- public/js/reporting.js
+++ public/js/reporting.js
@@ -546,8 +546,6 @@ Report.prototype.renderTimeline = function () {
         var metric = chartMetrics[c];
 
         if (!this.state.hiddenSeries || !this.state.hiddenSeries[metric.baseName]) {
-            colors.push(this.getColor(colorIndex).color);
-
             column = columnEnum[metric.name];
 
             if (column) {
