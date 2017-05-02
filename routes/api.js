0d0b79dff9027c539552c15f898da59966efa7fb
vizzaccaro <phil@netapplications.com>
2017-05-01 18:23:21 -0700

    session api

--- routes/api.js
+++ routes/api.js
@@ -26,7 +26,7 @@ function setup(app, application, callback) {
     app.post('/login', application.enforceSecure, function (req, res) {
         api.login(req.body['username'], req.body['password'], application.settings.appId, function (err, authObject) {
             if (!err)
-                api.setSessionCookie(res, authObject.accessToken);
+                req.session.auth = authObject;
             api.REST.sendConditional(res, err, null, 'success');
         });
     });
